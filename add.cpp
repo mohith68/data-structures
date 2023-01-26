153180b3411038e34674eb9adf200735475c5263
mohith68 <112928639+mohith68@users.noreply.github.com>
2023-01-26 21:51:29 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,add;
+	printf("enter element ");
+	scanf("%d",&a);
+	printf("enter second element");
+	scanf("%d",&b);
+	add=a+b;
+	printf("%d",add);
+	
+}
