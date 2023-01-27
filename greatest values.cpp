11b76bdd548b1f69f87b3747df9e60157797edd3
mohith68 <112928639+mohith68@users.noreply.github.com>
2023-01-27 09:39:04 +0530

    Add files via upload

---
+++ greatest values.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+int main()
+
+{
+
+int a,b;
+
+printf("enter the value of a and b:");
+
+scanf("%d,%d",&a,&b);
+
+if(a<b)
+
+{
+
+printf("a is greater than b");
+
+}
+
+else
+
+{
+
+printf("b is greater than a");
+
+}
+
+
