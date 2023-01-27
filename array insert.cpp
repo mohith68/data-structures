8a6c656ad96c95dbca4327a86f8ed22e1327b3ba
mohith68 <112928639+mohith68@users.noreply.github.com>
2023-01-27 09:36:22 +0530

    Add files via upload

---
+++ array insert.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int arr[10],a,i;
+	printf("enter the size of elements");
+	scanf("%d",&a);
+	printf("enter the elements");
+	for(i=0;i<10;i++)
+	{
+		scanf("%d",&arr[10]);
+	}
+	
+}
